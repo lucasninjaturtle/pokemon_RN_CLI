565a208b8ed652ea1b8dd75d0bf5b037715ddc92
Lucas <laguero90@gmail.com>
2021-04-12 14:34:30 -0300

    search scrren

--- Pokedex/src/screens/SearchScreen/SearchScreen.tsx
+++ Pokedex/src/screens/SearchScreen/SearchScreen.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function SearchScreen() {
+
+
     return (
-        <View>
+        <SafeAreaView style={{flex:1, marginHorizontal: 20}}>
             <Text>
-                
+                SEARCH SCREEN
             </Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
